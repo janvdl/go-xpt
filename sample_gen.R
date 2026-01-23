cd3f1a31d16f63cfc958eb8d0b67ae67f789a460
Jan van der Linde <jvdl@jvdl.me>
2026-01-23 20:34:33 +0000

    added a sample xpt dataset and code to create it

---
+++ sample_gen.R
@@ -0,0 +1,33 @@
+#	 go-xpt: an open-source, Go solution to reading/writing XPT (SAS Transport) files.
+#    Copyright (C) 2026  Jan van der Linde
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# ==========================================
+# Sample code to create a simple XPT dataset
+# ==========================================
+
+library(haven)
+
+chars <- rep("Hello world!", 5)
+nums <- runif(5)
+dates <- rep(date(), 5)
+
+df <- data.frame(
+    chars,
+    nums,
+    dates
+)
+
+haven::write_xpt(df, "sample.xpt", version = 5)
